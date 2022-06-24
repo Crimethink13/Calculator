dddb566cf0d17ef2495b7acfe57083858b24a2d8
Crimethink13 <m.connley@live.com.au>
2022-06-24 15:39:44 +1000

    Fixed some functions

--- app.js
+++ app.js
@@ -5,32 +5,38 @@ const equalsBtn = document.querySelector('.equals');
 const clearBtn = document.querySelector('.clear');
 const display = document.querySelector('.display');
 const historyScreen = document.querySelector('.history-screen');
-const currentScreen = document.querySelector('.current-number');
+const currentScreen = document.querySelector('.current-screen');
 let firstNumber = '';
-let operator = '';
+let operator = null;
 let secondNumber = '';
-let result = '';
 
 //? Clears the screen
-clearBtn.onclick = () => {
-	historyScreen.textContent = '';
-	currentScreen.textContent = '';
-};
+clearBtn.onclick = () => clearAll();
 
 //? Listen for number
 //? Display current number
 numberBtn.forEach((number) => {
 	number.addEventListener('click', () => {
+		if (currentScreen.textContent === 'Error') clearCurrent();
+		currentScreen.classList.remove('result', 'error');
 		currentScreen.textContent += number.textContent;
 	});
 });
 
+//TODO Fix function when operator is pressed multiple times
 //? Save the operator variable
 //? Listen for operator and display on screen
 operatorBtn.forEach((button) => {
 	button.addEventListener('click', () => {
+		currentScreen.classList.remove('result', 'error');
+		if (currentScreen.textContent === 'Error') {
+			clearCurrent();
+			return;
+		}
+		//? Evaluate current operation before setting new operator
+		if (operator !== null) evaluate();
 		operator = button.textContent;
-		setOperator();
+		setFirstNumber(button.textContent);
 		// clearCurrent();
 	});
 });
@@ -39,9 +45,11 @@ operatorBtn.forEach((button) => {
 equalsBtn.addEventListener('click', evaluate);
 
 //? Save the first number variable
-//? Save the operator variable
 //? Display first number and operator
-function setOperator() {
+function setFirstNumber(operation) {
+	if (operator !== null) {
+		operator = operation;
+	}
 	firstNumber = currentScreen.textContent;
 	historyScreen.textContent = `${firstNumber}${operator}`;
 	clearCurrent();
@@ -52,28 +60,42 @@ function clearCurrent() {
 	currentScreen.textContent = '';
 }
 
+function clearAll() {
+	historyScreen.textContent = '';
+	currentScreen.textContent = '';
+	firstNumber = '';
+	operator = null;
+	secondNumber = '';
+}
+
 //? Save the second number variable
 function evaluate() {
 	secondNumber = currentScreen.textContent;
+	if (firstNumber === '' || secondNumber === '') return;
+	if (operator === '÷' && secondNumber == 0) {
+		secondNumber = '';
+		currentScreen.classList.add('error');
+		return (currentScreen.textContent = 'Error');
+	}
 	historyScreen.textContent += `${secondNumber}`;
 	clearCurrent();
-	operate(firstNumber, operator, secondNumber);
-	currentScreen.textContent = result;
+	currentScreen.textContent = roundNumber(operate(firstNumber, operator, secondNumber));
+	currentScreen.classList.add('result');
+	operator = null;
+}
+
+function roundNumber(num) {
+	return Math.round(num * 1000) / 1000;
 }
 
 function operate(a, operator, b) {
 	a = Number(a);
 	b = Number(b);
-	if (operator === '+') {
-		result = a + b;
-	}
-	if (operator === '−') {
-		result = a - b;
-	}
-	if (operator === '×') {
-		result = a * b;
-	}
-	if (operator === '÷') {
-		result = a / b;
-	}
+	if (operator === '+') return a + b;
+
+	if (operator === '−') return a - b;
+
+	if (operator === '×') return a * b;
+
+	if (operator === '÷') return a / b;
 }
